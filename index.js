a9cb9cb28dfc70f9e650c9cf1167f43a89df0229
Chukwuemelie Oguchi <chukwuemelie4@gmail.com>
2018-02-16 05:16:21 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+// 1 begins
+var recipes = { peanut:'butter'}
+// 2 begins
+function updateObjectWithKeyAndValue(object, key, value){
+  object = { prop: 1 }
+  object[key]= value
+  return object
+}
+// 3 begins
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+//  object = { prop: 1 }
+  object[key]= value
+  return object
+}
+// 4 begins
+function deleteFromObjectByKey(object, key){
+console.log (object)
+  var newobject = Object.assign({}, object)
+  delete newobject[key]
+//  var object = { prop: 1 }
+  return object
+}
